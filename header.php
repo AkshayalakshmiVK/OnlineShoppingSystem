681af947ee29301326e45487c2955c8832202886
AkshayalakshmiVK <83702505+AkshayalakshmiVK@users.noreply.github.com>
2022-09-21 20:38:44 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,400 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"><!-- supporting latest ersion of IE11/Edge-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>AKRA - A one stop store for all your needs</title>
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet"><!-- We can change this font-->
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Slick -->
+		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+		<!-- nouislider -->
+		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+		<link type="text/css" rel="stylesheet" href="css/accountbtn.css"/>
+		
+		
+		
+         
+		
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    <style>
+        #navigation {
+          background: #FF4E50;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #F9D423, #FF4E50);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #F9D423, #FF4E50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+          
+        }
+        #header {
+  
+            background: #780206;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #061151, #780206);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #061151, #780206); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  
+        }
+        #top-header {
+              
+  
+            background: #870000;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #190A05, #870000);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #190A05, #870000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+        }
+        #Profile-body, #chooser-body{
+        	background: #5900b3;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #5900b3, #ff9999);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #5900b3, #ff9999); 
+            font-family: courier, helvetica;
+            color: white;/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        }
+        #footer {
+            background: #7474BF;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #348AC7, #7474BF);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #348AC7, #7474BF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+          color: #1E1F29;
+        }
+        #bottom-footer {
+            background: #7474BF;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #348AC7, #7474BF);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #348AC7, #7474BF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+          
+
+        }
+        .footer-links li a {
+          color: #1E1F29;
+        }
+        .mainn-raised {
+            
+            margin: -7px 0px 0px;
+            border-radius: 6px;
+            box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
+
+        }
+       
+        .glyphicon{
+    display: inline-block;
+    font: normal normal normal 14px/1 FontAwesome;
+    font-size: inherit;
+    text-rendering: auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    }
+    .glyphicon-chevron-left:before{
+        content:"\f053"
+    }
+    .glyphicon-chevron-right:before{
+        content:"\f054"
+    }
+        
+
+       
+        
+        </style>
+
+    </head>
+	<body>
+		<!-- HEADER -->
+		<header>
+			<!-- TOP HEADER -->
+			<div id="top-header">
+				<div class="container">
+					<ul class="header-links pull-right">
+						<li><a href="" data-toggle="modal" data-target="#chooser"><i class="fa fa-inr"></i>Chooser's pick</a></li>
+						<div class="modal fade" id="chooser" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-body" id="chooser-body">
+										<!-- product -->
+										<?php
+                    include 'db.php';
+								
+                    
+					$product_query = "SELECT * FROM maximumordered";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+             
+
+                        echo "
+				
+                        
+                                
+								<div class='product'>
+									<a href='product.php?p=$pro_id'><div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+										<div class='product-label'>
+											<span class='sale'>-30%</span>
+											<span class='new'>NEW</span>
+										</div>
+									</div></a>
+									<div class='product-body'>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>$990.00</del></h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>add to wishlist</span></button>
+											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>add to compare</span></button>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
+									</div>
+								</div>
+                               
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+										
+										<!-- /product -->
+									
+	</div>
+</div>
+</div>
+</div>
+						<li><?php
+                             include "db.php";
+                            if(isset($_SESSION["uid"])){
+                                $sql = "SELECT first_name FROM user_info WHERE user_id='$_SESSION[uid]'";
+                                $query = mysqli_query($con,$sql);
+                                $row=mysqli_fetch_array($query);
+                                
+                                echo '
+                               <div class="dropdownn">
+                                  <a href="#" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> HI '.$row["first_name"].'</a>
+                                  <div class="dropdownn-content">
+                                    <a href="" data-toggle="modal" data-target="#Profile"><i class="fa fa-user-circle" aria-hidden="true" ></i>My Profile</a>
+                                    <a href="logout.php"  ><i class="fa fa-sign-in" aria-hidden="true"></i>Log out</a>
+                                    
+                                  </div>
+                                </div>';
+
+                            }else{ 
+                                echo '
+                                <div class="dropdownn">
+                                  <a href="#" class="dropdownn" data-toggle="modal" data-target="#myModal" ><i class="fa fa-user-o"></i> My Account</a>
+                                  <div class="dropdownn-content">
+                                    <a href="" data-toggle="modal" data-target="#Modal_login"><i class="fa fa-sign-in" aria-hidden="true" ></i>Login</a>
+                                    <a href="" data-toggle="modal" data-target="#Modal_register"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a>
+                                    
+                                  </div>
+                                </div>';
+                                
+                            }
+                                             ?>
+                               
+                                </li>				
+					</ul>
+					
+				</div>
+			</div>
+			<!-- /TOP HEADER -->
+			
+			<div class="modal fade" id="Profile" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-body" id="Profile-body">
+							<?php
+							$sql = "SELECT * FROM user_info WHERE user_id='$_SESSION[uid]'";
+                            $query = mysqli_query($con,$sql);
+                            $row=mysqli_fetch_array($query);
+                            echo '<span style="color:#99ffff; font-size:20px"> Welcome to AKRA <br></span>';
+                            echo 'Firstname : '.$row["first_name"].'<br>Lastname : '.$row["last_name"].'<br>E-mail : '.$row["email"].'<br>Phone : '.$row["mobile"].'<br>Address : '.$row["address1"];
+                            $sql = "SELECT COUNT(*) AS count FROM orders WHERE user_id='$_SESSION[uid]' GROUP BY user_id";
+                            $query = mysqli_query($con,$sql);
+                            $row=mysqli_fetch_array($query);
+                            if (empty($row)){
+                            	echo '<br>Orders so far : 0';
+                            }
+                            else{
+                            echo '<br>Orders so far : '.$row["count"];
+                        }
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- MAIN HEADER -->
+			<div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+						
+							<div class="header-logo">
+								<a href="#" class="logo">
+								<font style="font-style:normal; color: #ffb3ff;font-family: courier">
+                                       <span style="font-size: 38px"> AKRA - </span><span style="color:#99ffff; font-size:28px"> A one stop store for all your needs</span>
+                                    </font>
+									
+								</a>
+							</div>
+						
+						<!-- /LOGO -->
+
+						<!-- SEARCH BAR 
+						<div class="col-md-6">
+							<div class="header-search">
+								<form>
+									<select class="input-select">
+										<option value="0">All Categories</option>
+										<option value="1">Men</option>
+										<option value="1">Women </option>
+									</select>
+									<input class="input" id="search" type="text" placeholder="Search here">
+									<button type="submit" id="search_btn" class="search-btn">Search</button>
+								</form>
+							</div>
+						</div>
+						 /SEARCH BAR -->
+
+						<!-- ACCOUNT -->
+						<div class="col-md-3 clearfix">
+							<div class="header-ctn">
+								
+
+								<!-- Cart -->
+								<div class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+										<i class="fa fa-shopping-cart"></i>
+										<span>Your Cart</span>
+										<div class="badge qty">0</div>
+									</a>
+									<div class="cart-dropdown"  >
+										<div class="cart-list" id="cart_product">
+										
+											
+										</div>
+										
+										<div class="cart-btns">
+												<a href="cart.php" style="width:100%;"><i class="fa fa-edit"></i>  Edit cart</a>
+											
+										</div>
+									</div>
+										
+									</div>
+								<!-- /Cart -->
+
+								<!-- Menu Toogle -->
+								<div class="menu-toggle">
+									<a href="#">
+										<i class="fa fa-bars"></i>
+										<span>Menu</span>
+									</a>
+								</div>
+								<!-- /Menu Toogle -->
+							</div>
+						</div>
+						<!-- /ACCOUNT -->
+					</div>
+					<!-- row -->
+				</div>
+				<!-- container -->
+			</div>
+			<!-- /MAIN HEADER -->
+		</header>
+		<!-- /HEADER -->
+		<nav id='navigation'>
+			<!-- container -->
+            <div class="container" id="get_category_home">
+                
+            </div>
+				<!-- responsive-nav -->
+				
+			<!-- /container -->
+		</nav>
+            
+
+		<!-- NAVIGATION -->
+		
+		<div class="modal fade" id="Modal_login" role="dialog">
+                        <div class="modal-dialog">
+													
+                          <!-- Modal content-->
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                              
+                            </div>
+                            <div class="modal-body">
+                            <?php
+                                include "login_form.php";
+    
+                            ?>
+          
+                            </div>
+                            
+                          </div>
+													
+                        </div>
+                      </div>
+                <div class="modal fade" id="Modal_register" role="dialog">
+                        <div class="modal-dialog" style="">
+
+                          <!-- Modal content-->
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                              
+                            </div>
+                            <div class="modal-body">
+                            <?php
+                                include "register_form.php";
+    
+                            ?>
+          
+                            </div>
+                            
+                          </div>
+
+                        </div>
+                      </div>
+		
